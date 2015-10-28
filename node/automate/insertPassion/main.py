c9f0c5ce27746845a5e754b46de3aaed5e1d3afd
Sayan Das <poke19962008@gmail.com>
2015-10-28 21:04:22 +0530

    automate passions insertion with python

---
+++ node/automate/insertPassion/main.py
@@ -0,0 +1,26 @@
+import pymongo, os, json
+
+os.chdir("../");
+
+configs = json.loads(open("pyConfigs.json").read())
+
+try:
+    con = pymongo.Connection(host=configs['mongo']['host'], port=configs['mongo']['port'])
+except:
+    print "Cannot connect with MongoDB."
+
+db = con['talhat']
+
+os.chdir("./insertPassion")
+passions = open("passions.txt", "r").read().split("\n")
+
+index = 0
+for passion in passions:
+    passion = passion.lower()
+    try:
+        db.passions.insert({"passion": passion, "_id": index})
+        index = index+1
+    except:
+        print "[error] "+ passion + " failed to insert."
+
+print "[success] Inserted " + str(index) + " of " + str(len(passions)) + " passions"
