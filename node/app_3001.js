eade9e68b6331042d600f5996df25b27b057536d
Sayan Das <poke19962008@gmail.com>
2015-10-30 21:34:16 +0530

    populate passions in seperate div

--- node/app_3001.js
+++ node/app_3001.js
@@ -22,6 +22,19 @@ app.get("/getPassionsforProfile", function (req, res){
   });
 });
 
+app.get("/getPassionsWithKeywords", function (req, res){ // Return keywords when the user start typing his/her passion
+  var query = getQuery(req);
+
+  mongo.getPassionWithKeywords( query.keyword, function result(err, docs){
+    if(err){
+      logger.log("info", "getPassionsWithKeywords | Mongo Retrieval Failed.");
+      res.send({"status": "serverFault"});
+  }
+    else res.send(docs);
+  });
+
+});
+
 app.listen(3001, function (){
   logger.log("info" ,"SERVER STARTED");
   console.log("Listening on port 3001");
--- profile/js/script.js
+++ profile/js/script.js
@@ -4,9 +4,15 @@ $(document).ready(function (){
 
   $.getJSON(nodeHost + "getPassionsforProfile/","", function (data){
     for (var i = 0; i < data.length; i++) {
-      var li = "<li><a href=\"../index.html\">#"+ data[i].passion +"  <span class=\"badge\">" + data[i].count +"</span></a></li>";
-
-      $("#side-menu").append(li);
+      var li = "<li style=\"padding-top: 10px; padding-right: 15px; padding-bottom: 10px; padding-left: 15px;\"><a href=\"../index.html\">#"+ data[i].passion +"  <span class=\"badge\">" + data[i].count +"</span></a></li>";
+      $("#passions-list").append(li);
     }
+  });
+});
+
+$("#passion-keyword").on("click keyup", function (){
+  var keyword = $("#passion-keyword").val();
+  $.getJSON(nodeHost + "getPassionsWithKeywords/", keyword, function(data){
+    // $("#passions")
   });
 });
