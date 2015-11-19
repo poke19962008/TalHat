cdfa12c32561b1f2c9bb306cc701d420e5272f27
Sayan Das <poke19962008@gmail.com>
2015-11-19 06:06:06 +0530

    added bio and trimmed rightProfile template

--- node/db/mongo.js
+++ node/db/mongo.js
@@ -26,15 +26,19 @@ var queries = {
   },
 
   "insertData": function (mail, password, passion, name, result){
+    var initBio =  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.";
+
     mongoClient.connect(url,  function (err, db){
       var cur = db.collection('user_details').insert({
         "mail": mail,
         "password": password,
         "passion": passion,
-        "name": name
+        "name": name,
+        "bio": initBio,
       }, function (err, results){
         result(err);
       });
+
 
       var cur = db.collection('passions').update({"passion": passion}, {$inc : {"count": 1}});
 
